82bfba1b1fd75d657d61aa4734d88ab8d1c987d4
nishint.goyal <nishint.goyal@CPC-nishi-0ZY6R>
2024-11-25 09:42:59 +0530

    Add project files.

---
+++ Abstract_Factory_Pattern/Abstract_Factory.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory_Pattern
+{
+    public interface IFood
+    {
+        string Eat();
+    }
+
+    class VegFood : IFood
+    {
+        public string Eat()
+        {
+            return "I only eats veg food";
+        }
+    }
+
+    class MilkFood : IFood
+    {
+        public string Eat()
+        {
+            return "Yes eat milky-creamy food ";
+        }
+
+    }
+
+    public interface IFastFood
+    {
+        public string Eat();
+    }
+
+    class VegFF : IFastFood //concreate product1
+    {
+
+        public string Eat()
+        {
+            return "yes also eat veg fast food";
+        }
+
+    }
+    class MilkFF : IFastFood //concreate Product2
+    {
+        public string Eat()
+        {
+            return "Yes also eat milky fast food also";
+        }
+
+    }
+
+    public interface IChef
+    {
+        IFood CreateFood();
+        IFastFood CreateFastFood();
+    }
+
+    // Concrete Factory 1
+    class VegChef : IChef
+    {
+        public IFood CreateFood()
+        {
+            return new VegFood();
+        }
+
+        public IFastFood CreateFastFood()
+        {
+            return new VegFF();
+        }
+    }
+
+    // Concrete Factory 2
+    class MilkChef : IChef
+    {
+        public IFood CreateFood()
+        {
+            return new MilkFood();
+        }
+
+        public IFastFood CreateFastFood()
+        {
+            return new MilkFF();
+        }
+    }
+
+    class FoodService
+    {
+        private readonly IFood _food;
+        private readonly IFastFood _fastFood;
+
+        public FoodService(IChef chef)
+        {
+            _fastFood = chef.CreateFastFood();
+            _food = chef.CreateFood();
+        }
+
+        public void ServeFood()
+        {
+            Console.WriteLine(_food.Eat());
+            Console.WriteLine(_fastFood.Eat());
+        }
+    }
+}
---
+++ Abstract_Factory_Pattern/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Abstract factory design pattern provides an interface for creating families of 
+ * related or dependent objects without specifying their concrete classes.
+ */
+
+using Abstract_Factory_Pattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Choose a chef (Veg/Milk):");
+        string choice = Console.ReadLine();
+
+        IChef chef;
+        switch (choice)
+        {
+            case "Veg":
+                chef = new VegChef();
+                break;
+            case "Milk":
+                chef = new MilkChef();
+                break;
+            default:
+                throw new ArgumentException("Invalid choice");
+        }
+
+        FoodService foodService = new FoodService(chef);
+        foodService.ServeFood();
+    }
+}
+
+
+
